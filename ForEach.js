1d5cc5837fe657d3a628a6c1abe0eb085921b797
farihaorna23 <farihaorna@gmail.com>
2021-10-01 19:39:07 -0400

    added code to myEach

---
+++ ForEach.js
@@ -0,0 +1,26 @@
+// FOR EACH //
+
+
+
+//applies function for each element in an array
+//takes elemnt, index and array as parameters
+//Return value= underdefined
+
+//the array that is being used for testing
+const arr=[1,2,3] 
+
+Array.prototype.myEach = function(callbackFn) {
+   for(let i=0; i<this.length; i++)
+   {
+       if(this[i]===undefined) continue;
+       callbackFn(this[i], i, this);
+   }
+};
+
+
+//test
+console.log("the result for myEach is:")
+arr.myEach((x,i,arr) => console.log(x,i,arr));
+//prints out the element in the arr and its index
+console.log("the result for forEach is:")
+arr.forEach((x,i,arr) => console.log(x,i,arr));

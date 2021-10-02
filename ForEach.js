1bebaab54b0cbbb99c6c3df34171decabbf3aef3
farihaorna23 <farihaorna@gmail.com>
2021-10-01 23:36:49 -0400

    tested the code and commeneted out everything other than the myEach function

--- ForEach.js
+++ ForEach.js
@@ -7,7 +7,7 @@
 //Return value= underdefined
 
 //the array that is being used for testing
-const arr=[1,2,3] 
+//const arr=[1,2,3] 
 
 Array.prototype.myEach = function(callbackFn) {
    for(let i=0; i<this.length; i++)
@@ -17,10 +17,11 @@ Array.prototype.myEach = function(callbackFn) {
    }
 };
 
-
+/*
 //test
 console.log("the result for myEach is:")
 arr.myEach((x,i,arr) => console.log(x,i,arr));
 //prints out the element in the arr and its index
 console.log("the result for forEach is:")
 arr.forEach((x,i,arr) => console.log(x,i,arr));
+*/

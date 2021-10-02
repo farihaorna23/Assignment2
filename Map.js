2dfcfc8aaa7342408877f3665aadd5f018b2f736
farihaorna23 <farihaorna@gmail.com>
2021-10-02 11:29:12 -0400

    tested code

--- Map.js
+++ Map.js
@@ -1,3 +1,5 @@
+//Map//
+
 /*Description: map calls a provided callbackFn function once for each element in an array, 
 in order, and constructs a new array from the results. callbackFn is invoked only for indexes 
 of the array which have assigned values.
@@ -24,18 +26,19 @@ Array.prototype.myMap = function(callbackFn) {
 console.log("The result for myMap is:");
 let result= array1.myMap((x,i,array1)=>(x*2)); //are they taking the other parameters because index and array1 are not highlighted.
 console.log(result);
+//[ 2, 8, 18, 32 ]
 
 console.log("The result for myMap is:");
 let result1= array1.myMap((x,i,array1)=>(x+3)); 
 console.log(result1);
-
+//[ 4, 7, 12, 19 ]
 
 console.log("The result for Map is:");
 console.log(array1.map((x,i,array1)=>(x*2))); 
-
+//[ 2, 8, 18, 32 ]
 
 console.log("The result for Map is:");
 console.log(array1.map((x,i,array1)=>(x+3))); 
-
+//[ 4, 7, 12, 19 ]
 
 

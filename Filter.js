6346e042988ab45e831e7498e22d24323df1ea6b
farihaorna23 <farihaorna@gmail.com>
2021-10-03 11:17:19 -0400

    tested the code more

--- Filter.js
+++ Filter.js
@@ -15,19 +15,17 @@ Return Value: A new array with the elements that pass the test.
 If no elements pass the test, an empty array will be returned.
 */
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-const filtered = [12, 5, 8, 130, 44];
+
 
 Array.prototype.myFilter = function(callbackFn) {
     const arr=[];
 
     for(let i=0; i<this.length; i++)
     {
-        if(this[i]===undefined) continue;
+        if(this[i]===undefined) continue; 
         {
-            callbackFn(this[i], i, this);
-        
-            if(callbackFn(this[i], i, this)==true) //should I put the if condition   if(this[i]===undefined) continue;
+                    
+            if(callbackFn(this[i], i, this)==true) 
             {
             
             arr.push(this[i]); 
@@ -40,6 +38,10 @@ Array.prototype.myFilter = function(callbackFn) {
 };
 
 //Test
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const filtered = [12, 5, 8, 130, 44];
+
 console.log("the result for myFilter is:");
 let result= words.myFilter((x)=> (x.length> 6));
 console.log(result);
@@ -48,11 +50,27 @@ console.log(result);
 console.log("the result for myFilter is:");
 let result1= filtered.myFilter((x)=> (x>10));
 console.log(result1);
+// filtered is [12, 130, 44]
 
 console.log("the result for filter is:");
 console.log(words.filter((x)=>(x.length> 6)));
+//[ 'exuberant', 'destruction', 'present' ]
 
 console.log("the result for filter is:");
 console.log(filtered.filter((x)=>(x> 10)));
 // filtered is [12, 130, 44]
+
+console.log("test3")
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+console.log(array.myFilter(isPrime)); // [2, 3, 5, 7, 11, 13]
 

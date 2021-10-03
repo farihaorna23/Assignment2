2c22c1bd9bed7c0b22b5dc291b931ad38b9e4435
farihaorna23 <farihaorna@gmail.com>
2021-10-03 15:34:22 -0400

    commented out everything but the mymap function

--- Map.js
+++ Map.js
@@ -10,35 +10,38 @@ Return Value: A new array with each element being the result of the callback fun
 */
 
 
-const array1 = [1, 4, 9, 16];
+
 Array.prototype.myMap = function(callbackFn) {
     const arr=[];
     for(let i=0; i<this.length; i++)
     {
-      if(this[i]===undefined) continue;{ //---okay to use?
+      if(this[i]===undefined) continue;{ 
         arr.push(callbackFn(this[i], i, this));
       }
     }
   return arr;
 };
 
+
+/*
 //Test
+const array1 = [1, 4, 9, 16];
 console.log("The result for myMap is:");
-let result= array1.myMap((x,i,array1)=>(x*2)); //are they taking the other parameters because index and array1 are not highlighted.
+let result= array1.myMap((x)=>(x*2));
 console.log(result);
 //[ 2, 8, 18, 32 ]
 
 console.log("The result for myMap is:");
-let result1= array1.myMap((x,i,array1)=>(x+3)); 
+let result1= array1.myMap((x)=>(x+3)); 
 console.log(result1);
 //[ 4, 7, 12, 19 ]
 
 console.log("The result for Map is:");
-console.log(array1.map((x,i,array1)=>(x*2))); 
+console.log(array1.map((x)=>(x*2))); 
 //[ 2, 8, 18, 32 ]
 
 console.log("The result for Map is:");
-console.log(array1.map((x,i,array1)=>(x+3))); 
+console.log(array1.map((x)=>(x+3))); 
 //[ 4, 7, 12, 19 ]
-
+*/
 
